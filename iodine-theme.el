6995ce7b292f2183cb93f5a0ea7f59f504d8bb5d
srdja <srdja.panic@gmail.com>
2015-10-20 02:55:38 +0200

    Updating boilerplate

--- iodine-theme.el
+++ iodine-theme.el
@@ -1,18 +1,35 @@
-;; Copyright (C) 2015 Srđan Panić <srdja.panic@gmail.com>
-;;
+;;; iodine-theme.el --- A light emacs color theme
+
+;; Copyright (C) 2015 Srđan Panić
+
+;; Author: Srđan Panić <srdja.panic@gmail.com>
+;; URL: https://github.com/srdja/iodine-theme
+;; Version 0.1
+;; Keywords: themes
+;; Package-Requires: ((emacs "24"))
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;;
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; Iodine is a light purplish emacs color theme
+
+;;; Usage:
+
+;; The theme can be loaded with M-x load-theme
+
+;;; Code:
 
 (deftheme iodine
   "Created 2015-10-20.")
@@ -87,3 +104,4 @@
  '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :width normal :height 98 :weight normal :slant normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground "#333333" :background "#FFFFFF" :stipple nil :inherit nil)))))
 
 (provide-theme 'iodine)
+;;; iodine-theme.el ends here
